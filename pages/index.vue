d718eda5d406c9b39d52261f64031998709cd01a
cagataycali <cagataycali@icloud.com>
2020-12-07 22:37:36 +0300

    Add more line

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h1><a href="https://github.com/cagataycali/find-mentor" target="_blank">Feel free to contribute!</a></h1>
     <h2>Exactly this project is 0km.</h2>
+    <h3>Every night & every deploy, the spread sheet will be parsed by GitHub actions, then generate this beauty.</h3>
     <ul>
       <li v-for="mentee in mentees" :key="mentee.slug">
         <NuxtLink :to="mentee.slug">
