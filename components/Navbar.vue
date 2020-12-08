b6e959cb57f84f050d69e0a9f1aec2690846df03
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-08 23:52:31 +0300

    Register Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,6 +15,10 @@
           <b-dropdown-item href="../#mentors">
               Mentors
           </b-dropdown-item>
+          <b-nav-item 
+          href="https://docs.google.com/forms/d/e/1FAIpQLSc3uWpEeBUCXMoGAJ5qm31p9URBppxXT5L4RJFrTOJee9TFjQ/viewform?usp=send_form" left>
+        Register
+      </b-nav-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-navbar>
