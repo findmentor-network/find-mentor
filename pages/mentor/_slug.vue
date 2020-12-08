375d907c46298e2edc5822ab20f9d163dfce167b
sevkioruc <sevkioruc34@gmail.com>
2020-12-08 10:33:42 -0800

    mentor ler icin http ile baslamayan url ler handle edildi

--- pages/mentor/_slug.vue
+++ pages/mentor/_slug.vue
@@ -5,13 +5,13 @@
       <ul>
         <li>Mentor: {{ doc.name }}</li>
         <li v-if="doc.twitter_handle.length">
-          <a :href="doc.twitter_handle">Twitter</a>
+          <a :href="isValidURL.twitter_handle">Twitter</a>
         </li>
         <li v-if="doc.github.length">
-          <a :href="doc.github">Github</a>
+          <a :href="isValidURL.github">Github</a>
         </li>
         <li v-if="doc.linkedin.length">
-          <a :href="doc.linkedin">Linkedin</a>
+          <a :href="isValidURL.linkedin">Linkedin</a>
         </li>
       </ul>
     </div>
@@ -19,15 +19,31 @@
 </template>
 
 <script>
-
 export default {
-  async asyncData ({ $content, params, error }) {
-    const [doc] = await $content('mentors').where({ slug: { $eq: params.slug } }).fetch()
+  computed: {
+    isValidURL() {
+      if (this.doc) {
+        !this.doc.github.startsWith("http")
+          ? (this.doc.github = `https://${this.doc.github}`)
+          : this.doc.github;
+        !this.doc.twitter_handle.startsWith("http")
+          ? (this.doc.twitter_handle = `https://${this.doc.twitter_handle}`)
+          : this.doc.twitter_handle;
+        !this.doc.linkedin.startsWith("http")
+          ? (this.doc.linkedin = `https://${this.doc.linkedin}`)
+          : this.doc.linkedin;
+      }
+      return this.doc;
+    },
+  },
+  async asyncData({ $content, params, error }) {
+    const [doc] = await $content("mentors")
+      .where({ slug: { $eq: params.slug } })
+      .fetch();
     if (!doc) {
-      return error({ statusCode: 404, message: 'Not found' })
+      return error({ statusCode: 404, message: "Not found" });
     }
-    return { doc }
-  }
-}
-
+    return { doc };
+  },
+};
 </script>
