2a5235765f6df9112b9b8b3440d840b11317f029
sevkioruc <sevkioruc34@gmail.com>
2020-12-08 11:55:38 -0800

    http ile baslamayan url ler handle edildi

--- pages/mentor/_slug.vue
+++ pages/mentor/_slug.vue
@@ -5,13 +5,13 @@
       <ul>
         <li>Mentor: {{ doc.name }}</li>
         <li v-if="doc.twitter_handle.length">
-          <a :href="isValidURL.twitter_handle">Twitter</a>
+          <a :href="fixProtocol(doc.twitter_handle)">Twitter</a>
         </li>
         <li v-if="doc.github.length">
-          <a :href="isValidURL.github">Github</a>
+          <a :href="fixProtocol(doc.github)">Github</a>
         </li>
         <li v-if="doc.linkedin.length">
-          <a :href="isValidURL.linkedin">Linkedin</a>
+          <a :href="fixProtocol(doc.linkedin)">Linkedin</a>
         </li>
       </ul>
     </div>
@@ -20,21 +20,10 @@
 
 <script>
 export default {
-  computed: {
-    isValidURL() {
-      if (this.doc) {
-        !this.doc.github.startsWith("http")
-          ? (this.doc.github = `https://${this.doc.github}`)
-          : this.doc.github;
-        !this.doc.twitter_handle.startsWith("http")
-          ? (this.doc.twitter_handle = `https://${this.doc.twitter_handle}`)
-          : this.doc.twitter_handle;
-        !this.doc.linkedin.startsWith("http")
-          ? (this.doc.linkedin = `https://${this.doc.linkedin}`)
-          : this.doc.linkedin;
-      }
-      return this.doc;
-    },
+  methods: {
+    fixProtocol (url) {
+      return url ? 'https://' + url.replace(/https?:\/\//gi, '') : 'javascript:void(0)'
+    }
   },
   async asyncData({ $content, params, error }) {
     const [doc] = await $content("mentors")
