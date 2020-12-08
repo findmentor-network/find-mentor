51885b46641397d86964136b8dbe04360d8f127f
Kemal Ayhan <kemalayhan013@gmail.com>
2020-12-08 15:40:39 +0300

    if statement for goals field

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -2,11 +2,12 @@
   <div class="container">
     <ul>
         <li>{{ doc.name }}</li>
-        <li><a :href="doc.twitter_handle ">Twitter</a></li>
-        <li><a :href="doc.github ">Github</a></li>
-        <li><a :href="doc.linkedin ">Linkedin</a></li>
+        <li><a :href="doc.twitter_handle">Twitter</a></li>
+        <li><a :href="doc.github">Github</a></li>
+        <li><a :href="doc.linkedin">Linkedin</a></li>
         <li>Interests: {{doc.interests}}</li>
-        <li>Goals: {{doc.goals}}</li>
+        <li v-if='doc.goals.length > 1'>Goals: {{doc.goals}}</li>
+        <li v-else>Goals: Unspecified</li>
         <li>Mentor: {{doc.mentor}}</li>
     </ul>
   </div>
