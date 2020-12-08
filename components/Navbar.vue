b838f51058f0cf2c4a48a56bb765af760785c283
cagataycali <cagataycali@icloud.com>
2020-12-09 00:08:37 +0300

    RM un-useds

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,10 +11,12 @@
         <b-nav-item href="/mentors/">
           Mentors
         </b-nav-item>
-        <b-nav-item 
-        href="https://docs.google.com/forms/d/e/1FAIpQLSc3uWpEeBUCXMoGAJ5qm31p9URBppxXT5L4RJFrTOJee9TFjQ/viewform?usp=send_form" left>
-        Register
-      </b-nav-item>
+        <b-nav-item
+          href="https://docs.google.com/forms/d/e/1FAIpQLSc3uWpEeBUCXMoGAJ5qm31p9URBppxXT5L4RJFrTOJee9TFjQ/viewform?usp=send_form"
+          rigth
+        >
+          Register
+        </b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
