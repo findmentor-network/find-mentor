614baafdd0b1ee337cd1cfc34e95f771e1f5fca9
Selçuk Tatar <41347478+tansionline@users.noreply.github.com>
2020-12-08 23:29:43 +0300

    Navbar inside Register Nav
    calisip calismadigini bilmiyorum, acaba dener misiniz?

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,6 +11,10 @@
         <b-nav-item href="/mentor/">
           Mentors
         </b-nav-item>
+        <b-nav-item 
+        href="https://docs.google.com/forms/d/e/1FAIpQLSc3uWpEeBUCXMoGAJ5qm31p9URBppxXT5L4RJFrTOJee9TFjQ/viewform?usp=send_form" left>
+        Register
+      </b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
