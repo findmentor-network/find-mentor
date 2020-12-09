356aa273a23e9cd13b0909c9084bc55142209399
Adem ÖZCAN <ozcanfurkan@gmail.com>
2020-12-09 15:02:45 +0300

    Twitter embed updated

--- components/person.vue
+++ components/person.vue
@@ -20,14 +20,16 @@
           <a :href="github" target="_blank">
             <button class="button github">
               <font-awesome-icon :icon="['fab', 'github']" color="white"/>
-              GitHub</button>
+              GitHub
+            </button>
           </a>
         </li>
         <li v-if="linkedin.length" class="links">
           <a :href="linkedin" target="_blank">
             <button class="button linkedin">
               <font-awesome-icon :icon="['fab', 'linkedin']" color="white"/>
-              LinkedIn</button>
+              LinkedIn
+            </button>
           </a>
         </li>
         <li v-if="interests && interests.length" class="text">
@@ -56,17 +58,24 @@
             />
           </a>
         </li>
-        <a class="twitter-timeline" :v-if="twitter.length" data-width="50%" :href="twitter +'?ref_src=twsrc%5Etfw'">Tweets
-          by
-          {{ name }}</a>
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <Timeline v-if="twitter.length" :id="twitterHandle" sourceType="profile" :options="{ tweetLimit: '5' }"/>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import { Timeline } from 'vue-tweet-embed'
+
 export default {
+  computed: {
+    twitterHandle () {
+      return this.twitter.split('twitter.com/')[1]
+    }
+  },
+  components: {
+    Timeline
+  },
   props: {
     slug: {
       type: String,
@@ -115,34 +124,42 @@ export default {
   flex-direction: column;
   align-items: center;
 }
+
 .avatar,
 .name,
 .links {
   margin-top: 15px;
 }
+
 .name {
   font-size: 27px;
 }
+
 .button {
   padding: 10px 30px;
   border: 2px solid transparent;
   border-radius: 4px;
   color: white;
 }
+
 .twitter {
   background-color: #00acee;
 }
+
 .github {
   background-color: #24292e;
 }
+
 .linkedin {
   background-color: #0e76a8;
 }
+
 .avatar {
   width: 250px;
   height: 250px;
   border-radius: 50%;
 }
+
 .text {
   font-size: 17px;
   margin-top: 10px;
