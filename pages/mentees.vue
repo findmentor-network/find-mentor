4b10a02284f78b10b252dd82db21238075630560
alpercun <alper-aga97@hotmail.com>
2020-12-09 03:48:01 +0300

    Implement mentee and mentor cards to search pages

--- pages/mentees.vue
+++ pages/mentees.vue
@@ -1,55 +1,51 @@
 <template>
   <div class="container">
     <h1>Mentees</h1>
-    <input v-model="filter" class="filter" placeholder="Filter Mentees">
+    <input v-model="filter" class="filter" placeholder="Filter Mentees" />
     <ul class="persons">
-      <li v-for="(mentee, index) in filteredMentees" :key="index" class="person">
-        <NuxtLink :to="'/mentee/' + mentee.slug">
-          <b-avatar :src="mentee.avatar" size="6rem" />
-          <h3 class="name">
-            {{ mentee.name }}
-          </h3>
-        </NuxtLink>
-        <p class="head">
-          Interests:
-        </p>
-        <p class="interestContent">
-          {{ mentee.interests }}
-        </p>
-      </li>
+      <MenteeCard
+        v-for="(mentee, index) in filteredMentees"
+        :key="index"
+        class="person"
+        :mentee="mentee"
+      />
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $content, params }) {
-    const mentees = await $content('mentees').fetch()
-    return { mentees }
+  async asyncData({ $content, params }) {
+    const mentees = await $content("mentees").fetch();
+    return { mentees };
   },
-  data () {
+  data() {
     return {
       filter: null
-    }
+    };
   },
   computed: {
-    filteredMentees () {
+    filteredMentees() {
       if (this.filter) {
-        return this.mentees.filter((mentee) => {
-          return this.filter.toLowerCase().split(' ').every(v => mentee.name.toLowerCase().includes(v))
-        })
+        return this.mentees.filter(mentee => {
+          return this.filter
+            .toLowerCase()
+            .split(" ")
+            .every(v => mentee.name.toLowerCase().includes(v));
+        });
       } else {
-        return this.mentees
+        return this.mentees;
       }
     }
   }
-}
+};
 </script>
 
 <style>
 * {
   box-sizing: border-box;
 }
+
 .filter {
   width: 40%;
   font-size: 14px;
@@ -58,33 +54,40 @@ export default {
   margin-bottom: 12px;
   margin-left: 35px;
 }
+
 .persons {
   list-style-type: none;
   width: 100%;
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
+  padding-left: 0;
 }
+
+.item {
+  width: 248px;
+  height: 320px;
+  background-color: #17aa90;
+  border-radius: 20px;
+  transition: box-shadow 0.3s;
+  border: 1px solid #ccc;
+  overflow: hidden;
+}
+
+.item:hover {
+  box-shadow: 16px 16px 16px rgba(11, 11, 11, 0.2);
+}
+
 .person {
   width: 250px;
   height: 250px;
-  align-items: start;
   text-align: center;
-  border: 1px solid grey;
-  border-radius: 4px;
-  padding-top: 10px;
-  margin: 20px 10px 0 0;
-  overflow: hidden;
+  margin: 30px 7.5px 0 7.5px;
 }
 .name {
   font-size: 26px;
-  color: chocolate;
 }
-.head {
-  color: #1d2124;
-  font-size: large;
-  margin-bottom: 0;
-  display: inline-flex;
-}
+
 .interestContent {
   display: -webkit-box;
   -webkit-line-clamp: 4;
--- pages/mentors.vue
+++ pages/mentors.vue
@@ -1,55 +1,51 @@
 <template>
   <div class="container">
     <h1>Mentors</h1>
-    <input v-model="filter" class="filter" placeholder="Filter Mentors">
+    <input v-model="filter" class="filter" placeholder="Filter Mentors" />
     <ul class="persons">
-      <li v-for="(mentor, index) in filteredMentors" :key="index" class="person">
-        <NuxtLink :to="'/mentor/' + mentor.slug">
-          <b-avatar :src="mentor.avatar" size="6rem" />
-          <h3 class="name">
-            {{ mentor.name }}
-          </h3>
-        </NuxtLink>
-        <p class="head">
-          Interests:
-        </p>
-        <p class="interestContent">
-          {{ mentor.interests }}
-        </p>
-      </li>
+      <MentorCard
+        v-for="(mentor, index) in filteredMentors"
+        :key="index"
+        class="person"
+        :mentor="mentor"
+      />
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData ({ $content, params }) {
-    const mentors = await $content('mentors').fetch()
-    return { mentors }
+  async asyncData({ $content, params }) {
+    const mentors = await $content("mentors").fetch();
+    return { mentors };
   },
-  data () {
+  data() {
     return {
       filter: null
-    }
+    };
   },
   computed: {
-    filteredMentors () {
+    filteredMentors() {
       if (this.filter) {
-        return this.mentors.filter((mentor) => {
-          return this.filter.toLowerCase().split(' ').every(v => mentor.name.toLowerCase().includes(v))
-        })
+        return this.mentors.filter(mentor => {
+          return this.filter
+            .toLowerCase()
+            .split(" ")
+            .every(v => mentor.name.toLowerCase().includes(v));
+        });
       } else {
-        return this.mentors
+        return this.mentors;
       }
     }
   }
-}
+};
 </script>
 
 <style>
 * {
   box-sizing: border-box;
 }
+
 .filter {
   width: 40%;
   font-size: 14px;
@@ -58,33 +54,40 @@ export default {
   margin-bottom: 12px;
   margin-left: 35px;
 }
+
 .persons {
   list-style-type: none;
   width: 100%;
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
+  padding-left: 0;
 }
+
+.mentor-item {
+  width: 248px;
+  height: 320px;
+  background-color: #17aa90;
+  border-radius: 20px;
+  transition: box-shadow 0.3s;
+  border: 1px solid #ccc;
+  overflow: hidden;
+}
+
+.mentor-item:hover {
+  box-shadow: 16px 16px 16px rgba(11, 11, 11, 0.2);
+}
+
 .person {
   width: 250px;
   height: 250px;
-  align-items: start;
   text-align: center;
-  border: 1px solid grey;
-  border-radius: 4px;
-  padding-top: 10px;
-  margin: 20px 10px 0 0;
-  overflow: hidden;
+  margin: 30px 7.5px 0 7.5px;
 }
 .name {
   font-size: 26px;
-  color: chocolate;
 }
-.head {
-  color: #1d2124;
-  font-size: large;
-  margin-bottom: 0;
-  display: inline-flex;
-}
+
 .interestContent {
   display: -webkit-box;
   -webkit-line-clamp: 4;
