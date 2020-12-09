d53536ef3aacf5b3cd12b8a30671a5ff6bf9fca7
alpercun <alper-aga97@hotmail.com>
2020-12-09 03:45:53 +0300

    Separate mentee and mentor card components Improve card design Add social media icons to cards component Fix css positioning issues

--- pages/index.vue
+++ pages/index.vue
@@ -2,57 +2,34 @@
   <div>
     <div class="container">
       <h1>
-        <a
-          href="https://github.com/cagataycali/find-mentor"
-          target="_blank"
-        >Feel free to contribute!</a>
+        <a href="https://github.com/cagataycali/find-mentor" target="_blank">
+          Feel free to contribute!
+        </a>
       </h1>
-      <h2>Exactly this project is 0km.</h2>
-      <h3>
+      <h4>Exactly this project is 0km.</h4>
+      <h4>
         Every night & every deploy, the spread sheet will be parsed by GitHub
         actions, then generate this beauty.
-      </h3>
-      <hr>
-      <h4>Mentors</h4>
+      </h4>
+      <hr />
+      <h2 class="title">Mentors</h2>
       <ul class="persons">
-        <li v-for="mentor in mentors" :key="mentor.slug" class="person">
-          <NuxtLink :to="'/mentor/' + mentor.slug">
-            <b-avatar
-              :src="mentor.avatar"
-              size="6rem"
-            />
-            <h3 class="name">
-              {{ mentor.name }}
-            </h3>
-          </NuxtLink>
-          <p class="head">
-            Interests:
-          </p>
-          <p class="interestContent">
-            {{ mentor.interests }}
-          </p>
-        </li>
+        <MentorCard
+          class="person"
+          v-for="mentor in mentors"
+          :key="mentor.slug"
+          :mentor="mentor"
+        />
       </ul>
-      <hr>
-      <h4>Mentees</h4>
+      <hr />
+      <h2 class="title">Mentees</h2>
       <ul class="persons">
-        <li v-for="mentee in mentees" :key="mentee.slug" class="person">
-          <NuxtLink :to="'/mentee/' + mentee.slug">
-            <b-avatar
-              :src="mentee.avatar"
-              size="6rem"
-            />
-            <h3 class="name">
-              {{ mentee.name }}
-            </h3>
-          </NuxtLink>
-          <p class="head">
-            Interest:
-          </p>
-          <p class="interestContent">
-            {{ mentee.interests }}
-          </p>
-        </li>
+        <MenteeCard
+          class="person"
+          v-for="mentee in mentees"
+          :key="mentee.slug"
+          :mentee="mentee"
+        />
       </ul>
     </div>
   </div>
@@ -60,12 +37,12 @@
 
 <script>
 export default {
-  async asyncData ({ $content, params }) {
-    const mentees = await $content('mentees').fetch()
-    const mentors = await $content('mentors').fetch()
-    return { mentees, mentors }
+  async asyncData({ $content, params }) {
+    const mentees = await $content("mentees").fetch();
+    const mentors = await $content("mentors").fetch();
+    return { mentees, mentors };
   }
-}
+};
 </script>
 <style>
 * {
@@ -75,34 +52,24 @@ export default {
   list-style-type: none;
   width: 100%;
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
+  padding-left: 0;
 }
 .person {
   width: 250px;
   height: 250px;
-  align-items: start;
   text-align: center;
-  border: 1px solid grey;
-  border-radius: 4px;
-  padding-top: 10px;
-  margin: 20px 10px 0 0;
-  overflow: hidden;
+  margin: 30px 7.5px 0 7.5px;
 }
 .name {
   font-size: 26px;
-  color: chocolate;
 }
-.head {
-  color: #1d2124;
-  font-size: large;
-  margin-bottom: 0;
-  display: inline-flex;
-}
+
 .interestContent {
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
-  padding: 0 3px;
   overflow: hidden;
 }
 </style>
