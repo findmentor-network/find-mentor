6c1fcc2d73b780388a0c7904687ebdd062f505b9
GitHub Action <action@github.com>
2020-12-09 21:48:45 +0000

    Add changes

--- docs/disqus.js
+++ docs/disqus.js
@@ -5,7 +5,8 @@
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
 
-const disqus_config = function () {
+// eslint-disable-next-line no-var
+var disqus_config = function () {
   this.page.url = window.location.href // Replace PAGE_URL with your page's canonical URL variable
   this.page.identifier = window.location.href // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
