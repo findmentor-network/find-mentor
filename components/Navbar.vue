bb09309c2925c51b5297a3b14fb48db6c8b93033
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-10 03:18:35 +0300

    try fix master

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,26 @@
 <template>
   <div>
-    <b-navbar>
+    <b-navbar type="dark" variant="primary">
       <b-navbar-nav>
         <b-nav-item href="/" left>
           Find Mentor
         </b-nav-item>
 
         <b-nav-item-dropdown text="Mentors" right>
-          <b-dropdown-item href="../#mentees">
-              Mentees
+          <b-dropdown-item href="#">
+            Mentees
           </b-dropdown-item>
-
-          
-          <b-dropdown-item href="../#mentors">
-              Mentors
+          <b-dropdown-item href="#">
+            Mentors
           </b-dropdown-item>
-          <b-nav-item 
-          href="https://docs.google.com/forms/d/e/1FAIpQLSc3uWpEeBUCXMoGAJ5qm31p9URBppxXT5L4RJFrTOJee9TFjQ/viewform?usp=send_form" left>
-        Register
-      </b-nav-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
-
 <script>
 export default {
-
 }
 </script>
-
 <style>
-
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,16 @@
 <template>
-  <div>
+  <div class="container">
     <Navbar />
-    <div class="container">
-      <h1><a href="https://github.com/cagataycali/find-mentor" target="_blank">Feel free to contribute!</a></h1>
-      <h2>Exactly this project is 0km.</h2>
-      <h3>Every night & every deploy, the spread sheet will be parsed by GitHub actions, then generate this beauty.</h3>
-      <h4>Mentors</h4>
-      <ul id="mentors" class="persons">
-        <li class="person" v-for="mentor in mentors" :key="mentor.slug">
-          <NuxtLink :to="'/mentor/'+mentor.slug">
-            <h3 class="name"> {{ mentor.name }}</h3>
-          </NuxtLink>
-          <p class="head">Interests:</p>
-          <p class="interestContent">{{mentor.interests}}</p>
-        </li>
-      </ul>
-      <h4>Mentees</h4>
-      <ul id="mentees" class="persons">
-        <li class="person" v-for="mentee in mentees" :key="mentee.slug">
-          <NuxtLink :to="'/mentee/'+mentee.slug">
-            <h3 class="name">{{ mentee.name }}</h3>
-          </NuxtLink>
-          <p class="head">Interest:</p>
-          <p class="interestContent">{{mentee.interests}}</p>
-        </li>
-      </ul>
-    </div>
+    <h1><a href="https://github.com/cagataycali/find-mentor" target="_blank">Feel free to contribute!</a></h1>
+    <h2>Exactly this project is 0km.</h2>
+    <h3>Every night & every deploy, the spread sheet will be parsed by GitHub actions, then generate this beauty.</h3>
+    <ul>
+      <li v-for="mentee in mentees" :key="mentee.slug">
+        <NuxtLink :to="mentee.slug">
+          {{ mentee.name }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -39,42 +24,3 @@ export default {
   }
 }
 </script>
-<style>
-* {
-  box-sizing: border-box;
-}
-.persons {
-  list-style-type: none;
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-}
-.person {
-  width:250px;
-  height: 250px;
-  align-items:start;
-  text-align:center;
-  border: 1px solid grey;
-  border-radius:4px;
-  padding-top:10px;
-  margin: 20px 10px 0 0;
-
-}
-.name{
-  font-size:26px;
-  color: chocolate;
-}
-.head{
-  color: #1d2124;
-  font-size: large;
-  margin-bottom:0;
-  display:inline-flex;
-}
-.interestContent{
-  display: -webkit-box;
-  -webkit-line-clamp: 4;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-}
-
-</style>
