34594fb9755374b4da7a6bfab4e2ba0f9fce4362
tansionline <2014513023@ogr.mehmetakif.edu.tr>
2020-12-10 04:03:46 +0300

    how to work page added

---
+++ pages/guide.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="jumbotron-mentor">
+    <b-jumbotron text-variant="black" border-variant="dark">
+      <template #header>
+        How to contact my mentor?
+      </template>
+      <hr class="my-4">
+      <h4>
+        1. First, want you wanna learn ? <br>
+        2. You should look <a class="links" href="https://findmentor.network/mentors/" target="_blank">mentors</a> page. <br>
+        3. Your mentor know your want? If yes; <br>
+        4. Contact he/she from Linkedin, Twitter or Mail. <br>
+        5. Last step: Join <a class="links" href="https://discord.com/invite/zkPb8P4B?utm_source=Discord%20Widget&utm_medium=Connect" target="_blank">Discord Server</a>
+      </h4>
+    </b-jumbotron>
+    <div>
+      <b-jumbotron text-variant="black" border-variant="dark">
+        <template #header>
+          How to contact my mentees?
+        </template>
+        <hr class="my-4">
+        <h4>
+          1. First join <a class="links" href="https://discord.com/invite/zkPb8P4B?utm_source=Discord%20Widget&utm_medium=Connect" target="_blank">discord server</a> and sure has mentor role (If you have not mentor role send message to admins). Or You can use Google Meet. <br>
+          2. Your mentees try to reach your social medias or mail. <br>
+          3. Say hi them then make decision about meeting (date, which discord room, etc). <br>
+          4. Last Step: Meeting. Be happy and help your Mentees.
+        </h4>
+      </b-jumbotron>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #jumbotron-mentor {
+        margin-top: 2%;
+    }
+</style>
