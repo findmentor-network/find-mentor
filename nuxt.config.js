c0a6d2f6cc4037254bec877abee6bbbac90000af
selimdoyranli <selim@radkod.com>
2020-12-10 20:43:58 +0300

    :package: add nuxt color mode module

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,8 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    // '@nuxtjs/eslint-module'
+    // '@nuxtjs/eslint-module',
+    '@nuxtjs/color-mode'
   ],
 
   /*
