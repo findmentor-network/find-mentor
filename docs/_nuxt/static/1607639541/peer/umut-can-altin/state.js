58d79ea1199d0d31fe49cfce55de89b5a6db5a1e
GitHub Action <action@github.com>
2020-12-10 22:45:30 +0000

    Add changes

--- docs/_nuxt/static/1607639541/peer/umut-can-altin/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1607639541",layout:"default",error:null,serverRendered:a,routePath:"\u002Fpeer\u002Fumut-can-altin",config:{content:{dbHash:"38b14dba"}},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
