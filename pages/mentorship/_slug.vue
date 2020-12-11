42e6b354bb0cfd6d6e56321b47555fc8566239b9
cagataycali <cagataycali@icloud.com>
2020-12-11 21:51:01 +0300

    Add mentorships detail

--- pages/mentorship/_slug.vue
+++ pages/mentorship/_slug.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Mentorship detail</div>
+  <div class="container">
+    (To-Do)
+    <h1>{{ doc.goal }}</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData ({ $content, params, error }) {
+    const [doc] = await $content('mentorships').where({ slug: { $eq: params.slug } }).fetch()
+    if (!doc) {
+      return error({ statusCode: 404, message: 'Not found' })
+    }
+    return { doc }
+  }
+}
+</script>
--- pages/mentorships.vue
+++ pages/mentorships.vue
@@ -1,22 +1,30 @@
 <template>
-      <div >
-<h3 v-for="mentor in mentorships" >
-        {{mentor.goal}}
-</h3>
-      </div>
+  <div class="container">
+    <h1>Active Mentorships</h1>
+    <p>This community, driven/developed by a fellow community.</p>
+    <p>As you can see, this project is the mentorship project. Developed by mentees.</p>
+    <p>You can find the mentors active mentorship campaigns below.</p>
+    <p>Pick one & contribute. You're mentee now.</p>
+    <ul>
+      <li v-for="mentorship in mentorships" :key="mentorship.slug">
+        <NuxtLink :to="`/mentorship/${mentorship.slug}`" itemprop="url">
+          {{ mentorship.goal }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  async fetch() {
-  this.mentorships = await this.$content('mentorships').fetch()
+  async fetch () {
+    this.mentorships = await this.$content('mentorships').fetch()
   },
- data() {
-    return{
-    mentorships: []
-}
-}
-    
+  data () {
+    return {
+      mentorships: []
+    }
+  }
+
 }
 </script>
-
