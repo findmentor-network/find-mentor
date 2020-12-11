14716247fc423c7de059f20f352599eeef86a10e
/c² <cagataycali@icloud.com>
2020-12-11 20:25:27 +0300

    Revert "dark mod düzenlemesi"

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,43 +1,53 @@
-
 <template>
-  <div>
-    <b-navbar
-      toggleable="lg"
-      v-bind:type="$colorMode.value"
-      v-bind:variant="$colorMode.value"
-    >
-      <b-navbar-brand to="/">Find Mentor</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-
-          <b-nav-item exact active-class="active" to="/mentees/">
-            Mentees
-          </b-nav-item>
-          <b-nav-item exact active-class="active" to="/mentors/">
-            Mentors
-          </b-nav-item>
-          <b-nav-item
-            href="https://forms.gle/pQwFZBXEyqBRDU2H9"
-            target="_blank"
-          >
-            Register
-          </b-nav-item>
-          <b-nav-item to="/guide/"> How It Works? </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-
+  <div class="app-navbar">
+    <b-navbar>
+      <b-navbar-nav>
+        <b-navbar-brand to="/" left>
+          Find Mentor
+        </b-navbar-brand>
+        <b-nav-item exact to="/mentees/">
+          Mentees
+        </b-nav-item>
+        <b-nav-item exact to="/mentors/">
+          Mentors
+        </b-nav-item>
+        <b-nav-item
+          href="https://forms.gle/pQwFZBXEyqBRDU2H9"
+          target="_blank"
+        >
+          Register
+        </b-nav-item>
+        <b-nav-item
+          to="/guide/"
+          active
+        >
+          How It Works?
+        </b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
-
 <script>
-export default {};
+export default {
+
+}
 </script>
 
 <style>
-</style>
+.app-navbar .navbar-light .navbar-brand {
+  color: var(--color-text-01)
+}
 
+.app-navbar .navbar-light .nav-link {
+  color: var(--color-text-02)
+}
+
+.app-navbar .navbar-light .nav-link:hover {
+  color: var(--color-text-03)
+}
+
+.app-navbar .navbar-light .nav-link.active {
+  color: var(--color-text-01)
+}
+</style>
