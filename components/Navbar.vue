daa37222d1b810792c7e044d44fa2d2fa45116ac
/c² <cagataycali@icloud.com>
2020-12-11 03:33:00 +0300

    Revert "Navbar  responsive düzeltmesi "

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,38 +1,37 @@
 <template>
-<div>
-  <b-navbar toggleable="lg" type="light" variant="light">
-    <b-navbar-brand to="/">Find Mentor</b-navbar-brand>
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
-
+  <div class="app-navbar">
+    <b-navbar>
       <b-navbar-nav>
-         <b-nav-item exact active-class="active" to="/mentees/">
-            Mentees
-          </b-nav-item>
-          <b-nav-item exact active-class="active" to="/mentors/">
-            Mentors
-          </b-nav-item>
-          <b-nav-item
-            href="https://forms.gle/pQwFZBXEyqBRDU2H9"
-            target="_blank">
-            Register
-          </b-nav-item>
-          <b-nav-item to="/guide/">
-            How It Works?
-          </b-nav-item>
+        <b-navbar-brand to="/" left>
+          Find Mentor
+        </b-navbar-brand>
+        <b-nav-item exact active-class="active" to="/mentees/">
+          Mentees
+        </b-nav-item>
+        <b-nav-item exact active-class="active" to="/mentors/">
+          Mentors
+        </b-nav-item>
+        <b-nav-item
+          href="https://forms.gle/pQwFZBXEyqBRDU2H9"
+          target="_blank"
+        >
+          Register
+        </b-nav-item>
+        <b-nav-item
+          to="/guide/"
+        >
+          How It Works?
+        </b-nav-item>
       </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
-
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+export default {
 
+}
+</script>
 
 <style>
 .app-navbar .navbar-light .navbar-brand {
