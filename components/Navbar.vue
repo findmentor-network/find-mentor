b4379d58f0e6a106732be2bce71616dfd8ecb520
ograsyilmaz <yilmazogras@outlook.com.tr>
2020-12-11 12:45:35 +0300

    Kod html tag eksikleri giderme

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,45 +1,42 @@
 
 <template>
-<div>
-  <b-navbar toggleable="lg"  v-bind:type=" $colorMode.value"  v-bind:variant=" $colorMode.value" >
-    <b-navbar-brand to="/">Find Mentor</b-navbar-brand>
+  <div>
+    <b-navbar
+      toggleable="lg"
+      v-bind:type="$colorMode.value"
+      v-bind:variant="$colorMode.value"
+    >
+      <b-navbar-brand to="/">Find Mentor</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav> 
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
 
-      <b-navbar-nav>
-        <b-navbar-brand to="/" left>
-          Find Mentor
-        </b-navbar-brand>
-        <b-nav-item exact active-class="active" to="/mentees/">
-          Mentees
-        </b-nav-item>
-        <b-nav-item exact active-class="active" to="/mentors/">
-          Mentors
-        </b-nav-item>
-        <b-nav-item
-          href="https://forms.gle/pQwFZBXEyqBRDU2H9"
-          target="_blank"
-        >
-          Register
-        </b-nav-item>
-        <b-nav-item
-          to="/guide/"
-        >
-          How It Works?
-        </b-nav-item>
-      </b-navbar-nav>
+          <b-nav-item exact active-class="active" to="/mentees/">
+            Mentees
+          </b-nav-item>
+          <b-nav-item exact active-class="active" to="/mentors/">
+            Mentors
+          </b-nav-item>
+          <b-nav-item
+            href="https://forms.gle/pQwFZBXEyqBRDU2H9"
+            target="_blank"
+          >
+            Register
+          </b-nav-item>
+          <b-nav-item to="/guide/"> How It Works? </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
 
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
-
 </style>
 
