ca9f1af5e0b724b22b0d2dde5b4e50ad66779ec0
GitHub Action <action@github.com>
2020-12-11 02:30:04 +0000

    Add changes

---
+++ docs/_nuxt/static/1607653798/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1607653798",layout:"default",error:null,serverRendered:a,routePath:"\u002F",config:{content:{dbHash:"3444d7fb"}},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
