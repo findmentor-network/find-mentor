f05089ddb2d0906151e86b981f2cabb528151f4a
GitHub Action <action@github.com>
2020-12-12 20:00:07 +0000

    Add changes

--- docs/_nuxt/static/1607802997/peer/cagatay-cali/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1607802997",layout:"default",error:null,serverRendered:a,routePath:"\u002Fpeer\u002Fcagatay-cali",config:{content:{dbHash:"1a99ddc5"}},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
