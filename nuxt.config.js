f33970abd6a96b5e866d0a8acd4fefa06e950add
cagatay.cali <cagatay.cali@trendyol.com>
2020-12-13 05:11:49 +0300

    Remove google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,6 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~plugins/ga.js', mode: 'client' },
     { src: '~plugins/qr.js' },
     { src: '~plugins/fa.js' },
     { src: '~plugins/vue-infinite-loading.js', mode: 'client' },
