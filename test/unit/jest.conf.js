e01d84728743815d42a96b144448ddc1feeebd1f
unknown <sevkioruc34@gmail.com>
2020-12-15 18:29:32 +0300

    Jest config düzenlendi

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,20 +1,18 @@
 module.exports = {
-  jest: {
-    moduleFileExtensions: [
-      "js",
-      "json",
-      "vue"
-    ],
-    transform: {
-      ".*\\.(vue)$": "vue-jest",
-      ".*\\.(js)$": "babel-jest"
-    },
-    setupFiles: ['<rootDir>/test/unit/setup'],
-    moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/src/$1"
-    },
-    collectCoverage: true,
-    collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"],
-    verbose: true,  
-  }
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  watchman: false,
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1',
+    '^~~/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/*.vue',
+  ],
 }
