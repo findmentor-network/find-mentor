d76f7b53d0fab0525f8de99cba966ba4c744e330
hasantezcan <hasantezcan77@gmail.com>
2020-12-28 16:06:26 +0300

    Redesign jobs pages

--- pages/jobs.vue
+++ pages/jobs.vue
@@ -1,45 +1,51 @@
 <template>
   <div id="jumbotron-mentor" class="container">
-    <h1 class="title">Jobs</h1>
-    <br />
-    <h3>
-      <a href="https://forms.gle/895gP7bu1AqTbXi1A">Add your job listing</a>
-    </h3>
-    <p>This community, driven/developed by a fellow community.</p>
-    <p>
-      As you can see, this project is the mentorship project. Developed by
-      mentees.
-    </p>
-    <p>You can list your job listing below for 30 days.</p>
-    <div v-for="job in jobs" :key="job">
-      <b-card no-body class="overflow-hidden">
-        <b-row no-gutters>
-          <b-col md="6">
-            <b-card-img
-              :src="job.logo"
-              :alt="job.company"
-              class="rounded-0"
-            ></b-card-img>
-          </b-col>
-          <b-col md="6">
-            <b-card-body :title="job.company + ' | ' + job.position">
-              <b-card-text>
-                {{ job.company }} | {{ job.position }}
-              </b-card-text>
-              <b-card-text>
-                {{ job.description }}
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
-      </b-card>
-    </div>
-    <h4>
-      <a
-        href="https://github.com/cagataycali/find-mentor/blob/master/pages/jobs.vue"
-        >Contribute this page</a
-      >
-    </h4>
+    <b-jumbotron text-variant="black" border-variant="dark">
+      <h1>Jobs</h1>
+      <p>
+        <i>
+          <p>
+            This community, driven/developed by a fellow community. <br />
+            As you can see, this project is the mentorship project. Developed by
+            mentees. <br />
+            You can list your job listing below for 30 days.
+          </p>
+        </i>
+        <a href="https://forms.gle/895gP7bu1AqTbXi1A"
+          ><b>Add your job listing</b></a
+        >
+      </p>
+    </b-jumbotron>
+    <b-card-group deck>
+      <div v-for="job in jobs" :key="job">
+        <b-card no-body class="overflow-hidden mb-4">
+          <b-row no-gutters>
+            <b-col md="4">
+              <b-img fluid :src="job.logo" :alt="job.company"></b-img>
+            </b-col>
+            <b-col md="8">
+              <b-card-body class="">
+                <b-card-title
+                  >{{ job.company }} | {{ job.position }}</b-card-title
+                >
+                <b-card-text>
+                  {{ job.description }}
+                </b-card-text>
+                <b-card-text> <b>Location: </b>{{ job.location }} </b-card-text>
+                <b-card-text>
+                  <a :href="job.address">{{ job.address }}</a>
+                </b-card-text>
+              </b-card-body>
+            </b-col>
+          </b-row>
+          <template #footer>
+            <small class="text-muted float-right"
+              ><b>Posting date: </b> {{ job.date }}</small
+            >
+          </template>
+        </b-card>
+      </div>
+    </b-card-group>
   </div>
 </template>
 
