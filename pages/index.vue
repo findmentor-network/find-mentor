9abe7302899f4d04b20e276c07787beedd7fb086
GitHub Action <action@github.com>
2021-01-04 22:17:29 +0000

    Add changes

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,7 @@
       <h1>
         <a href="https://github.com/cagataycali/find-mentor"> Feel free to contribute! </a>
       </h1>
-      <p class="pad-both">
-        Every night & every deploy, the spread sheet will be parsed by GitHub actions, then generate this beauty.
-      </p>
+      <p class="pad-both">Every night & every deploy, the spread sheet will be parsed by GitHub actions, then generate this beauty.</p>
       <div id="contribs">
         <template v-if="$fetchState.pending">
           <app-spinner class="d-block mx-auto my-5" />
@@ -29,7 +27,7 @@
       </NuxtLink>
     </header>
     <div class="container">
-      <hr>
+      <hr />
       <!-- Mentors -->
       <h2 class="title my-4">
         <NuxtLink to="/mentors/">
@@ -43,9 +41,7 @@
       <template v-else>
         <PersonList :persons="mentors" strict-type="mentors" />
       </template>
-      <NuxtLink class="text-center d-block mb-5" to="/mentors/">
-        🤳 Click here for all mentors
-      </NuxtLink>
+      <NuxtLink class="text-center d-block mb-5" to="/mentors/"> 🤳 Click here for all mentors </NuxtLink>
 
       <!-- Mentees -->
       <h2 class="title mb-4 mt-5">
@@ -60,20 +56,18 @@
       <template v-else>
         <PersonList :persons="mentees" strict-type="mentees" />
       </template>
-      <NuxtLink class="text-center d-block mb-5" to="/mentees/">
-        🤳 Click here for all mentees
-      </NuxtLink>
+      <NuxtLink class="text-center d-block mb-5" to="/mentees/"> 🤳 Click here for all mentees </NuxtLink>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  async fetch () {
+  async fetch() {
     const [info, page, { contribs }] = await Promise.all([
       this.$content('info').fetch(),
       this.$content('readme').fetch(),
-      this.$content('contribs').fetch()
+      this.$content('contribs').fetch(),
     ])
 
     const [mentors, mentees] = await Promise.all([
@@ -90,7 +84,7 @@ export default {
         .sortBy('registered_at', 'desc')
         .skip(Math.random() * (info.menteeCount - 0) + 0)
         .limit(16)
-        .fetch()
+        .fetch(),
     ])
 
     this.info = info
@@ -99,27 +93,27 @@ export default {
     this.mentors = mentors
     this.mentees = mentees
   },
-  data () {
+  data() {
     return {
       info: {},
       page: null,
       contribs: {},
       mentors: [],
       mentees: [],
-      isVisitedGuide: false
+      isVisitedGuide: false,
     }
   },
-  beforeMount () {
+  beforeMount() {
     this.checkGuideVisited()
   },
   methods: {
-    checkGuideVisited () {
+    checkGuideVisited() {
       const isVisited = window.localStorage.getItem('guideVisited')
 
       this.isVisitedGuide = isVisited
-    }
+    },
   },
-  head () {
+  head() {
     const title = 'Find Mentor & Mentees Network'
     const description = `${this.info.mentorCount || ''} mentor is mentoring ${this.info.menteeCount | ''} people, join us!`
     const icon = 'https://findmentor.network/icon.png'
@@ -129,56 +123,56 @@ export default {
         {
           hid: 'description',
           name: 'description',
-          content: description
+          content: description,
         },
         {
           hid: 'twitter:title',
           name: 'twitter:title',
-          content: title
+          content: title,
         },
         {
           hid: 'twitter:description',
           name: 'twitter:description',
-          content: description
+          content: description,
         },
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: icon
+          content: icon,
         },
         {
           hid: 'twitter:image:alt',
           name: 'twitter:image:alt',
-          content: description
+          content: description,
         },
         {
           hid: 'og:title',
           property: 'og:title',
-          content: title
+          content: title,
         },
         {
           hid: 'og:description',
           property: 'og:description',
-          content: description
+          content: description,
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          content: icon
+          content: icon,
         },
         {
           hid: 'og:image:secure_url',
           property: 'og:image:secure_url',
-          content: icon
+          content: icon,
         },
         {
           hid: 'og:image:alt',
           property: 'og:image:alt',
-          content: description
-        }
-      ]
+          content: description,
+        },
+      ],
     }
-  }
+  },
 }
 </script>
 
